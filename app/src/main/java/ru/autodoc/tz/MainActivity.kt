c563282c7c7aca8f4cfa385562225c33dc0fd902
Denis609 <den_kolesnikov_99@mail.ru>
2022-07-24 16:18:13 +0700

    refactoring navController

--- app/src/main/java/ru/autodoc/tz/MainActivity.kt
+++ app/src/main/java/ru/autodoc/tz/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
@@ -14,19 +15,36 @@ import ru.autodoc.tz.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        binding.navView.setupWithNavController(navController = navController)
+        setupNavController()
 
         KeyboardVisibilityEvent.registerEventListener(activity = this) {
             binding.navView.isVisible = !it
+        }
+    }
+
+    private fun setupNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        navController.setGraph(R.navigation.nav_graph)
+        binding.navView.setupWithNavController(navController = navController)
+        navController.let {
+            NavigationUI.setupWithNavController(
+                binding.navView,
+                it
+            )
+            binding.navView.setOnItemSelectedListener { item ->
+                NavigationUI.onNavDestinationSelected(item, it)
+                true
+            }
+            binding.navView.setOnItemReselectedListener { item ->
+                it.popBackStack(destinationId = item.itemId, inclusive = false)
+            }
         }
     }
 }
